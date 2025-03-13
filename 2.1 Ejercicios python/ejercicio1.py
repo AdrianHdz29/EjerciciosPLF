a6e101071434769e5241bd8f0cc3b369cde82d89
AdrianHdz29 <manuel.adrian.hernandez.moreno@gmail.com>
2025-03-13 16:54:07 -0500

    Delete 2.1 Ejercicios python directory
    Reorganizar archivos

--- 2.1 Ejercicios python/ejercicio1.py
+++
@@ -1,6 +0,0 @@
-nombre = input("¿Cuál es tu nombre completo? ")
-semestre = input("¿Qué semestre estas cursando actualmente? ")
-grupo = input("¿En qué grupo te encuentras? ")
-carrera = input("¿En qué carrera te encuentras inscrito? ")
-
-print("El estudiante " + nombre + " estudia la carrera de " + carrera + " en el " + semestre + "° en el grupo " + grupo)
